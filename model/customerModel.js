8a2cc0bebbabe76b7f63deb7aa29b2638748f5f8
kash2005 <kashmifernando3@gmail.com>
2023-11-08 10:50:05 +0530

    add changes

---
+++ model/customerModel.js
@@ -0,0 +1,8 @@
+export class CustomerModel{
+    constructor(customer_id,customer_name,customer_address,customer_contact) {
+        this.customer_id = customer_id;
+        this.customer_name = customer_name;
+        this.customer_address = customer_address;
+        this.customer_contact = customer_contact;
+    }
+}
